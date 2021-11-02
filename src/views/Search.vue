062bbd7694911cc2ff8e4241af9b2751dc7a9682
Robert Rybak <mail.robertrybak@gmail.com>
2021-11-02 22:27:04 +0100

    connect Api Nasa

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,77 @@
+/* eslint-disable quotes */
 <template>
-  <div class="home">
-    <h1>Spacer</h1>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <input
+        id="search"
+        name="search"
+        type="text"
+        v-model="searchValue"
+        @input="heandlerInput"
+      />
+
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{ item.data[0].description }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line quotes
+import axios from "axios";
+// eslint-disable-next-line quotes
+import debounce from "lodash.debounce";
+
+// eslint-disable-next-line quotes
+const API = "http://images-api.nasa.gov/search";
 export default {
   // eslint-disable-next-line quotes
-  name: "Home",
+  name: "Search",
+  data() {
+    return {
+      // eslint-disable-next-line quotes
+      searchValue: "",
+      results: [],
+    };
+  },
+  methods: {
+    heandlerInput: debounce(function () {
+      axios
+        .get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 500),
+  },
 };
 </script>
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  display: flex;
+  padding: 30px;
+  margin: 0;
+  flex-direction: column;
+  align-items: center;
+}
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+}
+.search label {
+  font-family: Montserrot, sans-serif;
+}
+.search input {
+  height: 30px;
+  border: 0;
+  border-bottom: 1px solid black;
+}
+</style>
